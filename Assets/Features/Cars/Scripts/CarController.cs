5d61b53e0c2a15c3c3921e10241bd0b8b23646f3
Jason Miller <jasonlloydmiller@gmail.com>
2015-08-09 22:06:11 -0400

    Ramps for testing

--- Assets/Features/Cars/Scripts/CarController.cs
+++ Assets/Features/Cars/Scripts/CarController.cs
@@ -14,11 +14,16 @@ public class CarController : MonoBehaviour
 	public WheelCollider FLWheelCollider = null;
 	public WheelCollider RLWheelCollider = null;
 	public WheelCollider RRWheelCollider = null;
-	
+
+	public GameObject FRWheel = null;
+	public GameObject FLWheel = null;
+	public GameObject RLWheel = null;
+	public GameObject RRWheel = null;
+
 
 	public DriveTrain DriveTrain = DriveTrain.FrontWheelDrive;
 	public float GasForce = 100;
-
+	public float BreakForce = 10;
 	public float MaxTurnAngle = 20.0f;
 
 	private float currentAngle = 0;
@@ -33,23 +38,59 @@ public class CarController : MonoBehaviour
 	{
 
 		//Gas needs to continually accelerate to a max velocity
-		float Gas = Input.GetAxis ("Vertical") * Time.fixedDeltaTime * GasForce; 
-
+		float gasBreak = Input.GetAxis ("Vertical") * Time.fixedDeltaTime; 
 		float turnAngle = Input.GetAxis ("Horizontal") * MaxTurnAngle / 2;
+        
+		if (gasBreak > 0) {
+			gasBreak *= GasForce;
+			switch (DriveTrain) {
+			case DriveTrain.FrontWheelDrive:
+				FRWheelCollider.motorTorque = gasBreak;
+				FLWheelCollider.motorTorque = gasBreak;
+				break;
+			case DriveTrain.RearWheelDrive:
+				RRWheelCollider.motorTorque = gasBreak;
+				RLWheelCollider.motorTorque = gasBreak;
+				break;
+			case DriveTrain.AllWheelDrive:
+				FRWheelCollider.motorTorque = gasBreak;
+				FLWheelCollider.motorTorque = gasBreak;
+				RRWheelCollider.motorTorque = gasBreak;
+				RLWheelCollider.motorTorque = gasBreak;
+				break;
+			}
+		} else {
+			gasBreak *= BreakForce;
 
-		switch (DriveTrain) {
-		case DriveTrain.FrontWheelDrive:
-			FRWheelCollider.motorTorque = Gas;
-			FLWheelCollider.motorTorque = Gas;
-			break;
-		case DriveTrain.RearWheelDrive:
-			break;
-		case DriveTrain.AllWheelDrive:
-			break;
+			//apply break
+			FRWheelCollider.brakeTorque = gasBreak;
+			FLWheelCollider.brakeTorque = gasBreak;
+			RRWheelCollider.brakeTorque = gasBreak;
+			RLWheelCollider.brakeTorque = gasBreak;            
 		}
+
 
 		FRWheelCollider.steerAngle = turnAngle; 
 		FLWheelCollider.steerAngle = turnAngle;
+
+		applyVisuals ();
+	}
+
+	private void applyVisuals ()
+	{
+		applyVisuals (FLWheelCollider, FLWheel);
+		applyVisuals (FRWheelCollider, FRWheel);
+		applyVisuals (RLWheelCollider, RLWheel);
+		applyVisuals (RRWheelCollider, RRWheel);
+	}
+
+	private void applyVisuals (WheelCollider collider, GameObject visual)
+	{
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose (out position, out rotation);
+		visual.transform.position = position;
+		visual.transform.rotation = rotation;
 	}
 
 	private float clampAngle (float angle)
